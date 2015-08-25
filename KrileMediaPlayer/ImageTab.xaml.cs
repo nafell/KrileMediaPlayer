b526dd2e3a5ce9116a28bf042551b46e379d9060
RainbowCrashie <nexus556.45@gmail.com>
2015-08-25 20:30:32 +0900

    Forgot to commit these

---
+++ KrileMediaPlayer/ImageTab.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace KrileMediaPlayer
+{
+    /// <summary>
+    /// Interaction logic for ImageTab.xaml
+    /// </summary>
+    public partial class ImageTab : TabItem
+    {
+        public ImageTab()
+        {
+            InitializeComponent();
+        }
+    }
+}
