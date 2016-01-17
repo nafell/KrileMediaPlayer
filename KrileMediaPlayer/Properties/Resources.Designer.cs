dbf518ccce01b28ac3f7faba35dcef2611561e3c
Star Swirl <nexus556.45@gmail.com>
2016-01-18 02:43:40 +0900

    App icon

--- KrileMediaPlayer/Properties/Resources.Designer.cs
+++ KrileMediaPlayer/Properties/Resources.Designer.cs
@@ -71,6 +71,16 @@ namespace KrileMediaPlayer.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon favicon {
+            get {
+                object obj = ResourceManager.GetObject("favicon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap Link {
